e0dcb084f76f7d5e30eba1292dbae0a95354ce22
Benson Wathigo <bensonwathigo21@gmail.com>
2024-03-26 17:00:19 +0300

    fIrst commit

---
+++ Dart/TechnicalAssignmentOOP.dart
@@ -0,0 +1,47 @@
+//Interface definition
+abstract class Lion {
+  void makeHunt();
+}
+
+class animal {
+  String type;
+  int age;
+
+  // constuctor
+ animal(this.age, this.type);
+
+void displayAnimalTraits() {
+    print('Mbwa kali is $age years old. She is $type kingpin.');
+  }
+
+}
+// Inheritance
+class puppy extends animal implements Lion {
+  String breed;
+  // constructor
+  puppy(this.breed, String type, int age) :super(age, type);
+
+// Override
+@override 
+void makeHunt () {
+  print('Lions kill for food. They rae canivores');
+}
+
+  //method
+  void DisplaypuppyInfo () {
+    print("My pupppy is ${breed}. Commonly known as ${type}. He is $age years old.");
+  }
+}
+
+void main (){
+  //Assign
+
+  puppy myPuppy =puppy("Kenyan", "bosco", 15);
+
+  //call function
+
+  myPuppy.displayAnimalTraits();
+  myPuppy.DisplaypuppyInfo();
+  myPuppy.makeHunt();
+  
+}
